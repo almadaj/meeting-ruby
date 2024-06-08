e5f7f8cb3eabd3df8e9c6696933da8b728fada03
Juliano Almada <almadaj@edu.unifor.br>
2024-06-07 23:50:55 -0300

    objeto date e time

---
+++ 05-advanced/001-date.rb
@@ -0,0 +1,26 @@
+require "date"
+
+puts Date.new(1994)
+puts Date.new(1994, 1)
+puts Date.new(1994, 1, 18)
+
+birthday = Date.new(1997, 8, 4)
+
+puts birthday.class
+puts birthday.year
+puts birthday.day
+puts birthday.month
+puts birthday.wday
+  # data.wday
+  # 0: Domingo
+  # 1: Segunda-feira
+  # 2: Terça-feira
+  # 3: Quarta-feira
+  # 4: Quinta-feira
+  # 5: Sexta-feira
+  # 6: Sábado
+puts birthday.monday?
+puts birthday.tuesday?
+puts birthday.wednesday?
+puts birthday.thursday?
+puts birthday.friday?
